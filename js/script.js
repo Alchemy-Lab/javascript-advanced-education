45720c0cfc3fcd878eaf90968b8a320e68d89b64
fixthefish <developer@studioalchemy.net>
2023-03-25 10:21:18 +0100

    Final UX, functions enhancements

--- js/script.js
+++ js/script.js
@@ -1,20 +1,45 @@
-const inputCategory = document.querySelector('.input-category'),
-	buttonFind = document.querySelector('.button-find')
+const searchBar = document.querySelector('.searchbar'),
+	buttonFind = document.querySelector('.button-find'),
+	buttonLucky = document.querySelector('.button-lucky')
 
 let baseUrl = 'https://openlibrary.org/subjects/'
 
 buttonFind.addEventListener('click', getBooksForCategory)
+buttonLucky.addEventListener('click', getLuckyBook)
 
-function getBooksForCategory(e) {
-	e.preventDefault()
-	fetch(baseUrl + inputCategory.value + '.json')
-		.then((res) => res.json())
-		.then((data) => parseData(data['works']))
-		.catch((err) => console.error(err))
+searchBar.addEventListener('keydown', (e) => {
+	if (e.key === 'Enter') {
+		getBooksForCategory()
+	}
+})
+
+// function getBooksForCategory() {
+// 	fetch(baseUrl + searchBar.value + '.json')
+// 		.then((res) => res.json())
+// 		.then((data) => parseData(data['works']))
+// 		.catch((err) => console.error(err))
+// }
+
+async function getBooksForCategory() {
+	try {
+		const res = await fetch(baseUrl + searchBar.value + '.json')
+		const data = await res.json()
+		const books = parseData(data['works'])
+	} catch (error) {
+		console.error(error)
+	}
 }
 
+function getLuckyBook() {}
+
 function parseData(data) {
-	data.forEach((book) => {
-		console.log(book.title)
-	})
+	if (Array.isArray(data)) {
+		if (data.length > 0) {
+			data.forEach((book) => {
+				console.log(book.title)
+			})
+		} else {
+			console.log('No book for this category')
+		}
+	}
 }
