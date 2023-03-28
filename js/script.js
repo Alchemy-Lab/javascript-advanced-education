e428baa4b7d61c1b2ed1b1a6473a204e445d6a14
fixthefish <developer@studioalchemy.net>
2023-03-28 11:05:07 +0200

    New functions, optimizations, error handling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const searchBar = document.querySelector('.searchbar'),
 	buttonFind = document.querySelector('.button-find'),
-	buttonLucky = document.querySelector('.button-lucky')
+	buttonLucky = document.querySelector('.button-lucky'),
+	bookList = document.querySelector('.book-list'),
+	cancelBtn = document.querySelector('.cancel-btn')
 
 let baseUrl = 'https://openlibrary.org/subjects/'
 
@@ -8,25 +10,26 @@ buttonFind.addEventListener('click', getBooksForCategory)
 buttonLucky.addEventListener('click', getLuckyBook)
 
 searchBar.addEventListener('keydown', (e) => {
+	cancelBtn.classList.remove('hide')
 	if (e.key === 'Enter') {
+		// cancelBtn.classList.add('hide')
 		getBooksForCategory()
 	}
 })
 
-// function getBooksForCategory() {
-// 	fetch(baseUrl + searchBar.value + '.json')
-// 		.then((res) => res.json())
-// 		.then((data) => parseData(data['works']))
-// 		.catch((err) => console.error(err))
-// }
+cancelBtn.addEventListener('click', () => {
+	searchBar.value = ''
+	cancelBtn.classList.add('hide')
+})
 
 async function getBooksForCategory() {
 	try {
-		const res = await fetch(baseUrl + searchBar.value + '.json')
+		const res = await fetch(baseUrl + searchBar.value.toLowerCase() + '.json')
 		const data = await res.json()
 		const books = parseData(data['works'])
 	} catch (error) {
-		console.error(error)
+		bookList.innerHTML = `<p class="error"></p>Sorry, an error occurred while fetching: ${error.message}</p>`
+		// console.error(error)
 	}
 }
 
@@ -35,11 +38,42 @@ function getLuckyBook() {}
 function parseData(data) {
 	if (Array.isArray(data)) {
 		if (data.length > 0) {
+			// let bookList_html = ''
+			bookList.innerHTML = ''
+			let div, cl, content
 			data.forEach((book) => {
-				console.log(book.title)
+				let authors = ''
+				book.authors.forEach((author) => {
+					authors += `${author.name}, `
+				})
+				authors = authors.substring(0, authors.length - 2)
+
+				div = createDomElement('div', 'book-block', '')
+				div.appendChild(createDomElement('h3', 'book-title', book.title))
+				div.appendChild(createDomElement('p', 'book-auth', authors))
+				div.appendChild(createDomElement('p', 'book-desc', ''))
+				bookList.appendChild(div)
+
+				// 	bookList_html += `
+				// 	<div class="book-block">
+				// 	<h3 class="book-title">${book.title}</h3>
+				// 	<p class="book-auth">${authors}</p>
+				// </div>
+				// 	`
 			})
+			// bookList.innerHTML = bookList_html
 		} else {
 			console.log('No book for this category')
 		}
 	}
+}
+
+function createDomElement(el, cl, content) {
+	const tag = document.createElement(el)
+	tag.classList.add(cl)
+	const text = document.createTextNode(content)
+	tag.appendChild(text)
+	return tag
+	//   var element = document.getElementById("new");
+	//   element.appendChild(tag);
 }
